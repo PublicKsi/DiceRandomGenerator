f4263094c37f8dc7dd5ae509c9d96ea4950b4c0d
PublicKsi <ksi.public.github@gmail.com>
2021-11-12 18:16:21 +0300

    Remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,7 @@
 #include <iostream>
 #include "dicerandomgenerator.h"
 
-//#include <random>
-
-
 int main(int argc, char** argv) {
-
-//    std::mersenne_twister_engine<std::uint_fast32_t, 32, 624, 397, 31,
-//                                 0x9908b0df, 11,
-//                                 0xffffffff, 7,
-//                                 0x9d2c5680, 15,
-//                                 0xefc60000, 18, 1812433253> engine;
-
-//    engine.seed(0);
 
     uint32_t seed = 0;
     int dice_count = 500;
@@ -26,7 +15,6 @@ int main(int argc, char** argv) {
     DiceRandomGenerator generator(seed);
     std::cout << "Dices: ";
     for(int i = 1; i <= dice_count; ++i) {
-
         int dice = generator.getDice();
         if (i < dice_count)
             std::cout << dice << ", ";
